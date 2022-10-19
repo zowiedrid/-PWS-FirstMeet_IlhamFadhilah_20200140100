461174209a9a3c3c4b14c4b41876d1eaf7bd4d89
Ilham  Fadhilah <ilham.fadhilah.ft20@mail.umy.ac.id>
2022-10-19 22:16:14 +0700

    commit pertama

---
+++ src/main/java/Kel6PABD/AppRentCar/AppRentCarApplication.java
@@ -0,0 +1,13 @@
+package Kel6PABD.AppRentCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRentCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRentCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kel6PABD/AppRentCar/AppRentCarApplicationTests.java
@@ -0,0 +1,13 @@
+package Kel6PABD.AppRentCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRentCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
